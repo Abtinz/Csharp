e61253e9c671eed84c00de9f41bfe88975c36cb6
Abtinz <abtinzandi@gmail.com>
2022-02-03 19:39:14 +0330

    Console out and readline

---
+++ Learning/Console/console/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //how to print in #C
+            Console.Out.WriteLine("#C Cource\nhttps://maktabkhooneh.org/course/آموزش مقدماتی سی شارپ\nplease enter your name: ");
+            //how to get input #C
+            string name = Console.ReadLine();
+            Console.Out.WriteLine("Name:"+name);
+        }
+    }
+}
