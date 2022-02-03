1381531d5acafc44013fcb73ea8b378474661d62
Abtinz <abtinzandi@gmail.com>
2022-02-03 19:50:05 +0330

    First official project of #C course

---
+++ projects/First Project/First Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace First_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Out.WriteLine("First Project");
+        }
+    }
+}
