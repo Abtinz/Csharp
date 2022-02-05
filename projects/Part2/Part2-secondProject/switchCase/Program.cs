39e42f3dbcfb5aa034df62c11718fd8393589ad5
Abtinz <abtinzandi@gmail.com>
2022-02-05 17:26:20 +0330

    switch case project final test and text

--- projects/Part2/Part2-secondProject/switchCase/Program.cs
+++ projects/Part2/Part2-secondProject/switchCase/Program.cs
@@ -10,6 +10,56 @@ namespace switchCase
     {
         static void Main(string[] args)
         {
+            Console.Write("Please enter your first number: ");
+            String firstinputsData = Console.ReadLine();
+            int firstNumber = int.Parse(firstinputsData);
+
+            Console.Write("Please enter your second number: ");
+            String secondinputsData = Console.ReadLine();
+            int secondNumber = int.Parse(secondinputsData);
+
+            Console.Write("Please enter your Function Character: ");
+            String thirdinputsData = Console.ReadLine();
+            char character = char.Parse(thirdinputsData);
+
+            int resault = 0;
+
+            switch (character)
+            {
+                case '+':
+                    resault = firstNumber + secondNumber;
+                    Console.WriteLine("Sum Resault: " + resault);
+                    break;
+
+                case '-':
+                    resault = firstNumber - secondNumber;
+                    Console.WriteLine("Minus Resault: " + resault);
+                    break;
+
+
+                case '*':
+                    resault = firstNumber * secondNumber;
+                    Console.WriteLine("Multipaction Resault: " + resault);
+                    break;
+
+                case '/':
+                    resault = firstNumber / secondNumber;
+                    Console.WriteLine("Devide Resault: " + resault);
+                    break;
+
+
+                default:
+                    Console.WriteLine("Wrong input!");
+                    break;
+
+
+            }
+
+          
+           
+            //exit
+            Console.WriteLine("press eny key to exit");
+            Console.ReadLine();
         }
     }
 }
