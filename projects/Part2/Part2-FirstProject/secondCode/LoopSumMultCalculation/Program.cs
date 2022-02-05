74f5374954968d498a30e6ab04d1e934ad6f8b45
Abtinz <abtinzandi@gmail.com>
2022-02-05 15:46:26 +0330

    Sum While

--- projects/Part2/Part2-FirstProject/secondCode/LoopSumMultCalculation/Program.cs
+++ projects/Part2/Part2-FirstProject/secondCode/LoopSumMultCalculation/Program.cs
@@ -10,6 +10,37 @@ namespace LoopSumMultCalculation
     {
         static void Main(string[] args)
         {
+            Console.Write("Please enter your number: ");
+            String inputsData = Console.ReadLine();
+            int enteredNumber = int.Parse(inputsData);
+
+            //while Section --> sum
+            int index = 1;
+            int whileSumResult = 0;
+            while (index <= enteredNumber)
+            {
+                whileSumResult += index;
+                index -= -1;
+            }
+
+            Console.WriteLine("While Sum Section:\n" + whileSumResult);
+
+            //while Section --> sum
+            int SecondIndex = 1;
+            int whileMultiplicationResult = 0;
+            while (index <= enteredNumber)
+            {
+                whileSumResult += index;
+                index -= -1;
+            }
+
+            Console.WriteLine("While Sum Section:\n" + whileSumResult);
+
+            
+
+            //exit
+            Console.WriteLine("press eny key to exit");
+            Console.ReadLine();
         }
     }
 }
