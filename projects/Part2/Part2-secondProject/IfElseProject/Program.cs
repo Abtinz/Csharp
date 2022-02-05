755d0529fe5a1ddf617c8a80fb7d3778feec8910
Abtinz <abtinzandi@gmail.com>
2022-02-05 17:09:17 +0330

    if and else project

--- projects/Part2/Part2-secondProject/IfElseProject/Program.cs
+++ projects/Part2/Part2-secondProject/IfElseProject/Program.cs
@@ -10,6 +10,52 @@ namespace IfElseProject
     {
         static void Main(string[] args)
         {
+            Console.Write("Please enter your first number: ");
+            String firstinputsData = Console.ReadLine();
+            int firstNumber = int.Parse(firstinputsData);
+
+            Console.Write("Please enter your second number: ");
+            String secondinputsData = Console.ReadLine();
+            int secondNumber = int.Parse(secondinputsData);
+
+            Console.Write("Please enter your Function Character: ");
+            String thirdinputsData = Console.ReadLine();
+            char character = char.Parse(thirdinputsData);
+
+            int resault = 0;
+
+            if(character == '+')
+            {
+                resault = firstNumber + secondNumber;
+                Console.WriteLine("Sum Resault: " + resault);
+            }
+
+            else if (character == '-')
+            {
+                resault = firstNumber - secondNumber;
+                Console.WriteLine("Minus Resault: " + resault);
+            }
+
+            else if (character == '*')
+            {
+                resault = firstNumber * secondNumber;
+                Console.WriteLine("Multipaction Resault: " + resault);
+            }
+
+            else if (character == '/')
+            {
+                resault = firstNumber / secondNumber;
+                Console.WriteLine("Devide Resault: " + resault);
+            }
+
+            else
+            {
+                Console.WriteLine("Wrong input!");
+            }
+
+            //exit
+            Console.WriteLine("press eny key to exit");
+            Console.ReadLine();
         }
     }
 }
