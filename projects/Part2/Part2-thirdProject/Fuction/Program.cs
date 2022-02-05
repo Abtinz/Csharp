872808cd12b7a2dd4ec03f1738e26b029499e52e
Abtinz <abtinzandi@gmail.com>
2022-02-05 16:36:34 +0330

    Function and values

--- projects/Part2/Part2-thirdProject/Fuction/Program.cs
+++ projects/Part2/Part2-thirdProject/Fuction/Program.cs
@@ -10,6 +10,26 @@ namespace Fuction
     {
         static void Main(string[] args)
         {
+            Console.Write("Please enter your first number: ");
+            String firstinputsData = Console.ReadLine();
+            int firstNumber = int.Parse(firstinputsData);
+
+            Console.Write("Please enter your second number: ");
+            String secondinputsData = Console.ReadLine();
+            int secondNumber = int.Parse(secondinputsData);
+
+            Console.Write("Please enter your third number: ");
+            String thirdinputsData = Console.ReadLine();
+            int thirdNumber = int.Parse(thirdinputsData);
+        }
+
+        //this mehtod will calculate sum of three number
+        static float SumFunction(float firstNumber , float secondNumber , float thirdNumber)
+        {
+
+            float sumResult = 0;
+            sumResult = firstNumber + secondNumber + thirdNumber;
+            return sumResult;
         }
     }
 }
