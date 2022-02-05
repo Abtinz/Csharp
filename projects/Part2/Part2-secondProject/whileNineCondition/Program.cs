1a438a2cfe7f5a3d1f507b08506dda68ccac51be
Abtinz <abtinzandi@gmail.com>
2022-02-05 17:14:03 +0330

    While and condition project

---
+++ projects/Part2/Part2-secondProject/whileNineCondition/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace whileNineCondition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int applicationTurns = 1;
+            while (applicationTurns < 11)
+            {
+                Console.Write("application Turn: " + applicationTurns);
+                Console.Write("Please enter your first number: ");
+                String firstinputsData = Console.ReadLine();
+                int firstNumber = int.Parse(firstinputsData);
+
+                Console.Write("Please enter your second number: ");
+                String secondinputsData = Console.ReadLine();
+                int secondNumber = int.Parse(secondinputsData);
+
+                Console.Write("Please enter your Function Character: ");
+                String thirdinputsData = Console.ReadLine();
+                char character = char.Parse(thirdinputsData);
+
+                int resault = 0;
+
+                if (character == '+')
+                {
+                    resault = firstNumber + secondNumber;
+                    Console.WriteLine("Sum Resault: " + resault);
+                }
+
+                else if (character == '-')
+                {
+                    resault = firstNumber - secondNumber;
+                    Console.WriteLine("Minus Resault: " + resault);
+                }
+
+                else if (character == '*')
+                {
+                    resault = firstNumber * secondNumber;
+                    Console.WriteLine("Multipaction Resault: " + resault);
+                }
+
+                else if (character == '/')
+                {
+                    resault = firstNumber / secondNumber;
+                    Console.WriteLine("Devide Resault: " + resault);
+                }
+
+                else
+                {
+                    Console.WriteLine("Wrong input!");
+                }
+
+                applicationTurns -= -1;
+            }
+            
+
+            //exit
+            Console.WriteLine("press eny key to exit");
+            Console.ReadLine();
+        }
+    }
+}
