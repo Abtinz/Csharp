3b0f6d42fe051a685be3a282f98e2d9458973137
Abtinz <abtinzandi@gmail.com>
2022-02-05 16:11:13 +0330

    For Section --> sum

--- projects/Part2/Part2-FirstProject/secondCode/ForLoopCode/Program.cs
+++ projects/Part2/Part2-FirstProject/secondCode/ForLoopCode/Program.cs
@@ -10,6 +10,18 @@ namespace ForLoopCode
     {
         static void Main(string[] args)
         {
+            Console.Write("Please enter your number: ");
+            String inputsData = Console.ReadLine();
+            int enteredNumber = int.Parse(inputsData);
+
+            //for Section --> sum
+            int forSumResult = 0;
+            for (int index = 1; index <= enteredNumber; index+=1)
+            {
+                forSumResult += index;
+            }
+
+            Console.WriteLine("for Sum Section:\n" + forSumResult);
         }
     }
 }
