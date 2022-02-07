37549011a503dbe51fc4f8e43fde8033d6017aa0
Abtinz <abtinzandi@gmail.com>
2022-02-07 23:11:47 +0330

    signUp

--- Learning/OnlineCourses/OnlineCourses/Program.cs
+++ Learning/OnlineCourses/OnlineCourses/Program.cs
@@ -8,8 +8,38 @@ namespace OnlineCourses
 {
     internal class Program
     {
+        public static string fullName;
+        public static string userName;
+        public static string email;
+        public static int phoneNumber;
+
+        public static string courseName;
+        public static string courseId;
+        public static string courseTeacher;
+
+        public static string feedBack;
+
         static void Main(string[] args)
         {
+            signUp();
+            
+            while (true)
+            {
+                Console.WriteLine("Full name :" + fullName);
+                Console.WriteLine("1) New Course");
+
+            }
+            
+        }
+
+        static void signUp()
+        {
+            Console.WriteLine("Please enter your name: ");
+            fullName = Console.ReadLine();
+            Console.WriteLine("Please enter your last name: ");
+            fullName += Console.ReadLine();
+
+          
         }
     }
 }
