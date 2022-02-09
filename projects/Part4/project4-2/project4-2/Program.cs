9355fe465bf61379a3a1e1f8e7384dedb201bb22
Abtinz <abtinzandi@gmail.com>
2022-02-09 22:53:17 +0330

    projct first codes

--- projects/Part4/project4-2/project4-2/Program.cs
+++ projects/Part4/project4-2/project4-2/Program.cs
@@ -10,6 +10,37 @@ namespace project4_2
     {
         static void Main(string[] args)
         {
+            Dictionary<string, int> map = new Dictionary<string, int>();
+
+            string newKey;
+            int newValue;
+
+            while (true)
+            {
+                Console.WriteLine("Press 1 : new key\nPress 2 : show map");
+                string choice = Console.ReadLine();
+                if (choice == "1")
+                {
+                    Console.Write("New Key: ");
+                    newKey = Console.ReadLine();
+                    newValue = newKey.Length;
+                    map.Add(newKey, newValue);
+                }
+                else if (choice == "2")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Wromg Input!\nPlease enter 1 or 2");
+                }
+            }
+
+            foreach(string key in map.Keys) {
+                int value; 
+                map.TryGetValue(key, out value);
+                Console.WriteLine(key , value); 
+            }
         }
     }
 }
