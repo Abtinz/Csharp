210f9d423b21defc53ab4ac0d9e88931dbaef311
Abtinz <abtinzandi@gmail.com>
2022-02-09 23:20:20 +0330

    queue

--- projects/Part4/project4-3/project4-3/Program.cs
+++ projects/Part4/project4-3/project4-3/Program.cs
@@ -10,6 +10,31 @@ namespace project4_3
     {
         static void Main(string[] args)
         {
+            Queue<int> queue = new Queue<int>();
+
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+
+            int queueNumber;
+
+            Console.WriteLine("Queue size :",queue.Count);
+            Console.WriteLine("Queue members :");
+            queueNumber = queue.Dequeue();
+            Console.Write(queueNumber+"  ");
+            queueNumber = queue.Dequeue();
+            Console.Write(queueNumber + "  ");
+            queueNumber = queue.Dequeue();
+            Console.Write(queueNumber + "  ");
+            queueNumber = queue.Dequeue();
+            Console.Write(queueNumber + "  ");
+            queueNumber = queue.Dequeue();
+            Console.Write(queueNumber + "  ");
+
+            Console.Write("\npress eny key to exit");
+            Console.ReadLine();
         }
     }
 }
