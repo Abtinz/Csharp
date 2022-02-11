1040a722af6dff8f780c4afcdc02be65f5646211
Abtinz <abtinzandi@gmail.com>
2022-02-11 21:22:28 +0330

    project3.1

---
+++ projects/Part3/project-1/project4_1/Triangle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project4_1
+{
+    internal class Triangle
+    {
+        private float TriangleBase { set; get; }
+        private float TriangleHeight { get; }
+
+        public Triangle(float BaseValue)
+        {
+            TriangleBase = BaseValue;
+        }
+
+        /**
+         * this method will print type of our shape
+         **/
+
+        public static void shapeType()
+        {
+            Console.WriteLine("Triangle");
+        }
+
+        /*
+         * this methid will calculate area of Triangle
+         */
+        public float AreaCalculator()
+        {
+            float area = (TriangleBase * TriangleHeight)/2;
+            return area;
+        }
+     }
+}
