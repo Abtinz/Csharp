078ee8b6ced804e2d2dc0723a69c66a7728bedd6
Abtinz <abtinzandi@gmail.com>
2022-02-11 21:57:33 +0330

    second project of third section

---
+++ projects/Part3/project-2/project3_1/Animal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project3_1
+{
+    internal class Animal
+    {
+    }
+}
