08f36c99da3f8d1472209a444909777cf3777f9f
Abtinz <abtinzandi@gmail.com>
2022-02-12 15:37:43 +0330

    new project 3.3 for overloading

---
+++ projects/Part3/project-3/project3/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project3
+{
+    internal class Calculator
+    {
+        public float result { get; set; }
+
+        public void Average(float firstNumber , float secondNumber) {
+             result = (firstNumber + secondNumber)/2; 
+        }
+
+        public void Average(float firstNumber, float secondNumber , float thirdNumber)
+        {
+            result = (firstNumber + secondNumber + thirdNumber) / 3;
+        }
+
+        public void Average(float firstNumber, float secondNumber, float thirdNumber,float fourthNumber)
+        {
+            result = (firstNumber + secondNumber + thirdNumber + fourthNumber) / 4;
+        }
+
+    }
+}
---
+++ projects/Part3/project-3/project3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            List<int> numbers = new List<int>();
+            while(numbers.Count < 5)
+            {
+                if (numbers.Count < 2)
+                {
+                    Console.WriteLine("Please enter a new number");
+                    numbers.Add(int.Parse(Console.ReadLine()));
+                }
+                else
+                {
+                    Console.WriteLine("Numbers Count: " + numbers.Count +
+                                      "\nyou can enter four or three or two number to average!\n" +
+                                      "if you want to exit type exit:(if you dont please press enter)" );
+                    string choice = Console.ReadLine();
+                    if (choice == "exit")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter a new number");
+                        numbers.Add(int.Parse(Console.ReadLine()));
+                    }
+                    
+                }
+                
+            }
+        }
+    }
+}
