9534b227467bec68d26243e412dad8de23796635
Abtinz <abtinzandi@gmail.com>
2022-02-14 16:46:20 +0330

    Alg project structure

---
+++ projects/Part5/Data Structure and Algorithm(Amature)/project5.1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project5._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int number;
+                Console.WriteLine("Please enter your number:\n" +
+                                  "If you want to exit please write: Exit" );
+                string enterdMassage  = Console.ReadLine();
+                if (enterdMassage == "Exit")
+                {
+                    break;
+                }
+                if (int.TryParse(enterdMassage, out number))
+                {
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input!");
+
+                }
+            }
+        }
+    }
+}
