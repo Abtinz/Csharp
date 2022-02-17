4fe054d8806be5bfa5c401c279bbe68bc851c767
Abtinz <abtinzandi@gmail.com>
2022-02-17 20:07:35 +0330

    interface

---
+++ projects/Part3/project-5/interfaceProject/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interfaceProject
+{
+    interface IMover
+    {
+        void moving();
+    }
+
+    internal class Human : IMover
+    {
+        public void moving()
+        {
+            Console.WriteLine("Human is moving!");
+        }
+    }
+
+    internal class Plane : IMover
+    {
+        public void moving()
+        {
+            Console.WriteLine("Plane is moving!");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Human human = new Human(); 
+            Plane plane = new Plane();
+            human.moving();
+            plane.moving();
+            Console.WriteLine("press eny key to exit");
+            Console.ReadLine();
+        }
+    }
+}
