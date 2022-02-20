c0083192508b8d676ea3dcd9b83187b825e8d6d0
Abtinz <abtinzandi@gmail.com>
2022-02-20 17:49:31 +0330

    Files

---
+++ projects/Part6/project1/FileIOStream/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIOStream
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string context = "";
+            StreamReader InputFile = new StreamReader(@"C:\Users\ABTIN\Documents\coding\Programming\#C Programing\Maktab Khoneh\projects\Part6\project1\TestFile.txt");
+            while (!InputFile.EndOfStream)
+            {
+                context +=  InputFile.ReadLine() + "\r\n";
+            }
+            Console.WriteLine("TextFile context:\n"+context);
+            StreamWriter NewFile = new StreamWriter(@"C:\Users\ABTIN\Documents\coding\Programming\#C Programing\Maktab Khoneh\projects\Part6\project1\NewFile.txt");
+            NewFile.WriteLine(context);
+            NewFile.Close();
+            Console.WriteLine("Press any key to Exit!");
+            Console.ReadLine();
+        }
+    }
+}
