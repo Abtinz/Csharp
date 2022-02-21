ed48fee9daf7ec34f0dc72cb0c70d1a75dc2cb7e
Abtinz <abtinzandi@gmail.com>
2022-02-21 21:34:09 +0330

    Try Catch

---
+++ projects/Part6/project2/tryCatchProject/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tryCatchProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("If you want to exit please write \"EXIT\"");
+            while (true)
+            {
+                Console.Write("Please enter your number: ");
+                string enterdStatement = Console.ReadLine();
+
+                if(enterdStatement == "EXIT")
+                {
+                    break;
+                }
+                else
+                {
+                    try
+                    {
+                        int number = int.Parse(enterdStatement);
+                        Console.WriteLine("Entered Number: "+number + "\nresult(2*number): "+ (number*2));
+                    }catch (Exception exeption)
+                    {
+                        Console.WriteLine("Enterd value is invalid!\nplease type a number or write \"EXIT\" to exit");
+                    }
+                }
+            }
+        }
+    }
+}
